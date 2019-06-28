29608d0d80fcbc3462d829583356fd4987662329
w8DREAD <dreadgodik@gmail.com>
2019-06-28 15:33:44 +0300

    push

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const bodyParser = require('body-parser');
+const app = express();
 
-/* GET home page. */
+const urlEncodedParser = bodyParser.urlencoded({extended: false});
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Заметки'});
 });
+
+
+router.get('/features', function(req, res, next) {
+  res.render('features', { features: "Тут будут фичи"});
+});
+
+
+router.get('/news', function(req, res, next) {
+  res.render('news', { news: "Тут будут новости"});
+});
+
 
 module.exports = router;
