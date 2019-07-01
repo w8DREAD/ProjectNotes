7e668d5efab374bb593d3e27889c213516b73e14
w8DREAD <dreadgodik@gmail.com>
2019-07-01 11:51:31 +0300

    commit

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,20 @@
-const express = require('express');
-const router = express.Router();
-const bodyParser = require('body-parser');
-const app = express();
+const express = require('express')
+const router = express.Router()
+const bodyParser = require('body-parser')
+const app = express()
 
-const urlEncodedParser = bodyParser.urlencoded({extended: false});
+const urlEncodedParser = bodyParser.urlencoded({ extended: false })
 
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Заметки'});
-});
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Заметки', style_main: '#FF0000' })
+})
 
+router.get('/features', function (req, res, next) {
+  res.render('features', { features: 'Тут будут фичи', style_features: '#FF0000' })
+})
 
-router.get('/features', function(req, res, next) {
-  res.render('features', { features: "Тут будут фичи"});
-});
+router.get('/news', function (req, res, next) {
+  res.render('news', { news: 'Тут будут новости', style_news: '#FF0000' })
+})
 
-
-router.get('/news', function(req, res, next) {
-  res.render('news', { news: "Тут будут новости"});
-});
-
-
-module.exports = router;
+module.exports = router
