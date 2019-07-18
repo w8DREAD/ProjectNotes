ea838e2cb9ce71a79adae812740755208277ed7b
w8DREAD <dreadgodik@gmail.com>
2019-07-18 11:42:37 +0300

    commit

--- public/script/script.js
+++ public/script/script.js
@@ -3,16 +3,23 @@ window.addEventListener('click', function (e) {
     if (e.target.parentNode.className == 'close button') {
       const xhr = new XMLHttpRequest()
       let data = e.target.id
+      console.log(e)
       xhr.open('delete', 'notes/' + data, true)
       xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded')
       let main = document.querySelector('div.content')
-      let post = document.getElementById(`post-${data}`)
-      main.removeChild(post)
-      xhr.send()
+      let post = e.path.find(cur => {
+        if (cur.attributes.class) {
+          if (cur.attributes.class.value == 'articles-news') {
+            return cur
+          }
+        }
+      })
+      // main.removeChild(post)
+      // xhr.send()
     }
-    if (e.target.parentNode.className == 'input-group-text') {
-      console.log('123')
-    }
+    // if (e.target.parentNode.className == 'input-group-text') {
+    //   console.log('123')
+    // }
   })
 })
 
@@ -22,21 +29,23 @@ window.addEventListener('click', function (e) {
       const xhr = new XMLHttpRequest()
       let data = e.target.id
       const text = document.getElementById('note-' + data)
-      text.insertAdjacentHTML('afterbegin', `<div contenteditable="true"> = "${text.children[0].innerText}"> </div>`)
-      // text.removeChild(text.children[0])
-      const editMode = text.classList.contains('editMode')
-      if (editMode) {
-
-      }
-      xhr.open('put', 'notes/' + data, true)
-      xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded')
-      xhr.send()
+      text.setAttribute('contenteditable', 'true')
+    //   if (contenteditable) {
+    //
+    //   }
+    //   xhr.open('put', 'notes/' + data, true)
+    //   xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded')
+    //   xhr.send()
     }
-    if (e.target.parentNode.className == 'input-group-text') {
-      console.log('123')
-    }
+    console.log('123')
   })
 })
+
+// window.addEventListener('click', e => {
+//   if (e.target.patentNode.className == 'input-group-text') {
+// let comment = document.getElementById()
+//   }
+// })
 
 // if (xhr.status === 200) {
 //   console.log('result', xhr.responseText)
