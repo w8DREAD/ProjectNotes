215cf5bae2858fb04363c28355e3a64424a067e9
w8DREAD <dreadgodik@gmail.com>
2019-08-15 13:31:10 +0300

    re

--- mongodb/mongo.js
+++ mongodb/mongo.js
@@ -1,16 +1,15 @@
-// const MongoClient = require('mongodb').MongoClient;
-//
-// const url = 'mongodb://localhost:27017/';
-// const mongoClient = new MongoClient(url, { useNewUrlParser: true });
-//
-//
-// const dbo = new Promise((resolve, reject) => {
-//   mongoClient.connect((err, dbo) => {
-//     if (err) reject(err);
-//     resolve(dbo.db('usersdb'));
-//   });
-// });
-//
+const MongoClient = require('mongodb').MongoClient;
+
+const url = 'mongodb://localhost:27017/';
+const mongoClient = new MongoClient(url, { useNewUrlParser: true });
+
+const dbo = new Promise((resolve, reject) => {
+  mongoClient.connect((err, dbo) => {
+    if (err) reject(err);
+    resolve(dbo.db('usersdb'));
+  });
+});
+
 // const take = (collection, findKey) => new Promise(async (resolve, reject) => {
 //   const db = await dbo;
 //   const mySort = { raiting: -1 };
@@ -21,12 +20,12 @@
 // });
 //
 //
-// async function save(collection, data) {
-//   const db = await dbo;
-//   db.collection(collection).insert(data, (err, res) => {
-//     if (err) throw err;
-//   });
-// }
+async function save(collection, data) {
+  const db = await dbo;
+  db.collection(collection).insert(data, (err, res) => {
+    if (err) throw err;
+  });
+}
 //
 // const update = (collection, where, newValue) => new Promise(async (resolve, reject) => {
 //   const db = await dbo;
@@ -41,6 +40,7 @@
 //   db.collection(collection).drop();
 // }
 //
-// module.exports = {
-//   take, save, update, drop,
-// };
+module.exports = {
+  // take, update, drop,
+  save,
+};
